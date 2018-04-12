d1aaec1eb5c12de07b9f8f091db8cb5ae3612bd0
Keely Brennan <keelybrennan@Keelys-MacBook-Pro-2.local>
2018-04-11 20:44:18 -0500

    act-8

--- Saturday_4.7.18/act-8/act-8.js
+++ Saturday_4.7.18/act-8/act-8.js
@@ -45,14 +45,13 @@ var starter = [];
 var sub = [];
 var team = [];
 
-//Once all of the players have been created, print their stats.
-var newPlayer = function(){
+var createPlayer = function(){
     if (starter.length + sub.length < 8) {
         console.log("make a new player");
         inquirer.prompt ([
             {
                 name: "name",
-                message: "{layer's name:",
+                message: "Player's name:",
             }, {
                 name: "position", 
                 message: "Player's position?",
@@ -78,69 +77,28 @@ var newPlayer = function(){
                     }
                     return false;
                 }
+            }
             ]).then(function(answers){
-    
-
+                var player = new Player(answers.name, answers.position, pataseInt(answers.offense), parseInt(answers.defense));
+            
+                //Once your code is functioning properly, move on to create a function called "playGame" 
+            //which will be run after all of the players have been created and will do the following:
+            //Run 5 times. Each time the function runs:    
+                if (starter.length < 5){ 
+                    starter.push(player);
+                    team.push(player);
+                    console.log(player.name + "added to starters");
+                }else {
+                    sub.push(player);
+                    team.push(player);
+                    console.log(player.name + "added to subs");   
+                }
+                createPlayer();
             });
-        
-        }else {
-        console.log("done");
-
-
-
-
-
-
-    }
-}
-
-
-
-//Once your code is functioning properly, move on to create a function called "playGame" 
-//which will be run after all of the players have been created and will do the following:
-//Run 5 times. Each time the function runs:
-    //Two random numbers between 1 and 20 are rolled and compared against the starters' offensive and defensive stats
-    
-    //If the first number is lower than the sum of the team's offensive stat, add one point to the team's score.
-    //If the second number is higher than the sum of the team's defensive stat, remove one point from the team's score.
-    
-    //After the score has been changed, prompt the user to allow them to substitute 1 player from within the starters array with the player from within the subs array.
-
-
-var count = 0;
-var playGame = function(){
-    if (count < 5){
-
-        if (Math.floor(Math.random() * 2) === 0) {
-
-        }else {
-        
+    }else {
+        for (var i=0; i < team.length; i++){
+            team[i].printStats();   
         }
-
-}
-};
-
-   
-//After the game has finished (been run 5 times):
-    //If the score is positive, run `goodGame` for all of the players currently within the starters array.
-    //If the score is negative, run `badGame` for all of the players currently within the starters array.
-    //If the score is equal to zero, do nothing with the starters.
-    //Give the user a message about if they won, and the status of their starters.
-
-
-//After printing the results, ask the user if they would like to play again.
-    
-    //Run `playGame` from the start once more if they do.
-    //End the program if they don't.
-    if (  ){
-        playGame();
-    }else{
-        console.log("End of Game!");
     }
 };
-playGame();
-
-
-
-//* HINT: Remember to use recursion when looping with inquirer! Otherwise your program might not return the prompts as you expect.
-//* HINT: It has been a while since we have worked with random numbers explicitly. If you are having troubles, look up Math.random on Google and you should find some resources to help.
+    createPlayer();
